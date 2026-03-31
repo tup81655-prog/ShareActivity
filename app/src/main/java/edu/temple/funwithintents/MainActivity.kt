1d99cf4f9aad6be0cbbff4e7ab014eaaadd3fc4d
tup81655-prog <tup81655@temple.edu>
2026-03-31 12:21:29 -0400

    update

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,8 +1,11 @@
 package edu.temple.funwithintents
 
+
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +18,23 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            val textToShare = editText.text.toString().trim()
 
+            if (textToShare.isNotEmpty()) {
+                AlertDialog.Builder(this).apply {
+                    setTitle("Share")
+                    setMessage("Do you want to share this text with a contact?")
+                    setPositiveButton("Yes") { _, _ ->
+                        val smsIntent = Intent(Intent.ACTION_SENDTO).apply {
+                            data = android.net.Uri.parse("smsto:") // This opens SMS app
+                            putExtra("sms_body", textToShare)     // Pre-fill message
+                        }
+                        startActivity(smsIntent)
+                    }
+                    setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }
+                    show()
+                }
+            }
         }
     }
 }
